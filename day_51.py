28d68725ee8c368cbeca2fc5a94ac50db8c6ad08
Amit Terdal <amitterdal2906@gmail.com>
2025-03-18 22:40:34 +0530

    Add files via upload

---
+++ day_51.py
@@ -0,0 +1,53 @@
+# 2206. Divide Array Into Equal Pairs
+
+# You are given an integer array nums consisting of 2 * n integers.
+
+# You need to divide nums into n pairs such that:
+
+# Each element belongs to exactly one pair.
+# The elements present in a pair are equal.
+# Return true if nums can be divided into n pairs, otherwise return false.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,3,2,2,2]
+# Output: true
+# Explanation: 
+# There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+# If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Explanation: 
+# There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy every condition.
+ 
+
+# Constraints:
+
+# nums.length == 2 * n
+# 1 <= n <= 500
+# 1 <= nums[i] <= 500
+
+
+# Solution: 
+
+class Solution(object):
+    def divideArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Sort the array to group equal elements together
+        nums.sort()
+
+        # Check consecutive pairs in the sorted array
+        for pos in range(0, len(nums), 2):
+            # If any pair doesn't match, we can't form n equal pairs
+            if nums[pos] != nums[pos + 1]:
+                return False
+
+        # All pairs found successfully
+        return True
