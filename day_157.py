3fab0274cc1ca1eb011be10479d9f1b9f9bcc895
Amit Terdal <amitterdal2906@gmail.com>
2025-07-06 08:21:53 -0400

    Add files via upload

---
+++ day_157.py
@@ -0,0 +1,59 @@
+# 3304. Find the K-th Character in String Game I
+
+# Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+
+# You are given a positive integer k.
+
+# Now Bob will ask Alice to perform the following operation forever:
+
+# Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+# For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+
+# Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+
+# Note that the character 'z' can be changed to 'a' in the operation.
+
+ 
+
+# Example 1:
+
+# Input: k = 5
+
+# Output: "b"
+
+# Explanation:
+
+# Initially, word = "a". We need to do the operation three times:
+
+# Generated string is "b", word becomes "ab".
+# Generated string is "bc", word becomes "abbc".
+# Generated string is "bccd", word becomes "abbcbccd".
+# Example 2:
+
+# Input: k = 10
+
+# Output: "c"
+
+ 
+
+# Constraints:
+
+# 1 <= k <= 500
+
+
+# Solution: 
+
+
+class Solution(object):
+    def kthCharacter(self, k):
+        """
+        :type k: int
+        :rtype: str
+        """
+        sb = "a"
+        while len(sb) < k:
+            size = len(sb)
+            for i in range(size):
+                next_char = chr(ord('a') + ((ord(sb[i]) - ord('a') + 1) % 26))
+                sb += next_char
+        return sb[k - 1]
