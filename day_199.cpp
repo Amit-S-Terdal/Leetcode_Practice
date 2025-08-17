333c8626a2f00d74b366524488dcdf23522f8868
Amit Terdal <amitterdal2906@gmail.com>
2025-08-17 01:30:54 -0400

    Add files via upload

---
+++ day_199.cpp
@@ -0,0 +1,62 @@
+// 2264. Largest 3-Same-Digit Number in String
+
+// You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+// It is a substring of num with length 3.
+// It consists of only one unique digit.
+// Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
+// Note:
+
+// A substring is a contiguous sequence of characters within a string.
+// There may be leading zeroes in num or a good integer.
+ 
+
+// Example 1:
+
+// Input: num = "6777133339"
+// Output: "777"
+// Explanation: There are two distinct good integers: "777" and "333".
+// "777" is the largest, so we return "777".
+// Example 2:
+
+// Input: num = "2300019"
+// Output: "000"
+// Explanation: "000" is the only good integer.
+// Example 3:
+
+// Input: num = "42352338"
+// Output: ""
+// Explanation: No substring of length 3 consists of only one unique digit. Therefore, there are no good integers.
+ 
+
+// Constraints:
+
+// 3 <= num.length <= 1000
+// num only consists of digits.
+
+
+// Solution:
+
+class Solution {
+    public:
+        string largestGoodInteger(string& num) {
+            int count=0;
+            char prev='X', maxd=' ';
+            for(char c: num){
+                if (c==prev) count++;
+                else count=1;
+                if (count==3)
+                    maxd=max(maxd, c);
+                prev=c;
+            }
+            return (maxd==' ')?"":string(3, maxd);
+        }
+    };
+    auto init = []()
+    { 
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        return 'c';
+    }();
