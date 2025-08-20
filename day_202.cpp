7e0b1a9ca1e3afd11ffb41fdfcc19df3c1a3d117
Amit Terdal <amitterdal2906@gmail.com>
2025-08-20 10:39:48 -0400

    Add files via upload

---
+++ day_202.cpp
@@ -0,0 +1,62 @@
+
+
+
+
+// 837. New 21 Game
+
+// Alice plays the following game, loosely based on the card game "21".
+
+// Alice starts with 0 points and draws numbers while she has less than k points. During each draw, she gains an integer number of points randomly from the range [1, maxPts], where maxPts is an integer. Each draw is independent and the outcomes have equal probabilities.
+
+// Alice stops drawing numbers when she gets k or more points.
+
+// Return the probability that Alice has n or fewer points.
+
+// Answers within 10^-5 of the actual answer are considered accepted.
+
+ 
+
+// Example 1:
+
+// Input: n = 10, k = 1, maxPts = 10
+// Output: 1.00000
+// Explanation: Alice gets a single card, then stops.
+// Example 2:
+
+// Input: n = 6, k = 1, maxPts = 10
+// Output: 0.60000
+// Explanation: Alice gets a single card, then stops.
+// In 6 out of 10 possibilities, she is at or below 6 points.
+// Example 3:
+
+// Input: n = 21, k = 17, maxPts = 10
+// Output: 0.73278
+ 
+
+// Constraints:
+
+// 0 <= k <= n <= 10^4
+// 1 <= maxPts <= 10^4
+
+
+// Solution: 
+
+
+
+class Solution {
+    public:
+        static double new21Game(int n, int k, int maxPts) {
+            if (k==0 || n>=k+maxPts-1) return 1;// edge case
+            vector<double> dp(n+1, 0);
+            dp[0]=1;
+            double wsum=1, prob=0;
+            // sliding window with window size maxPts
+            for (int l=0, r=1; r<=n; r++) {
+                dp[r]=wsum/maxPts;
+                (r<k)?wsum+=dp[r]:prob+=dp[r];
+                if (r>=maxPts) 
+                    wsum-=dp[l++];//move l
+            }
+            return prob;
+        }
+    };
