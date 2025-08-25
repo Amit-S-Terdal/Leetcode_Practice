2302aac2e77cd2267f99b9b695ed672ab140b7b8
Amit Terdal <amitterdal2906@gmail.com>
2025-08-25 07:50:28 -0400

    Add files via upload

---
+++ day_207.py
@@ -0,0 +1,68 @@
+# 3195. Find the Minimum Area to Cover All Ones I
+
+# You are given a 2D binary array grid. Find a rectangle with horizontal and vertical sides with the smallest area, such that all the 1's in grid lie inside this rectangle.
+
+# Return the minimum possible area of the rectangle.
+
+ 
+
+# Example 1:
+
+# Input: grid = [[0,1,0],[1,0,1]]
+
+# Output: 6
+
+# Explanation:
+
+
+
+# The smallest rectangle has a height of 2 and a width of 3, so it has an area of 2 * 3 = 6.
+
+# Example 2:
+
+# Input: grid = [[1,0],[0,0]]
+
+# Output: 1
+
+# Explanation:
+
+
+
+# The smallest rectangle has both height and width 1, so its area is 1 * 1 = 1.
+
+ 
+
+# Constraints:
+
+# 1 <= grid.length, grid[i].length <= 1000
+# grid[i][j] is either 0 or 1.
+# The input is generated such that there is at least one 1 in grid.
+
+
+# Solution: 
+
+
+class Solution(object):
+    def minimumArea(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        n, m = len(grid), len(grid[0])
+        iMin, iMax = float('inf'), -1
+        jMin, jMax = float('inf'), -1
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 0:
+                    continue
+                iMin = min(iMin, i)
+                iMax = max(iMax, i)
+                jMin = min(jMin, j)
+                jMax = max(jMax, j)
+
+        # If there are no 1s in the grid
+        if iMax == -1:
+            return 0
+
+        return (iMax - iMin + 1) * (jMax - jMin + 1)
