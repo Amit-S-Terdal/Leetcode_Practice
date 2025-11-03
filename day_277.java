2bd47f978ae1d935941b404520475c2355f2c89a
Amit Terdal <amitterdal2906@gmail.com>
2025-11-03 12:49:06 +0000

    Pushed through bash

---
+++ day_277.java
@@ -0,0 +1,62 @@
+// 1578. Minimum Time to Make Rope Colorful
+
+// Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where colors[i] is the color of the ith balloon.
+
+// Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the rope.
+
+// Return the minimum time Bob needs to make the rope colorful.
+
+ 
+
+// Example 1:
+
+
+// Input: colors = "abaac", neededTime = [1,2,3,4,5]
+// Output: 3
+// Explanation: In the above image, 'a' is blue, 'b' is red, and 'c' is green.
+// Bob can remove the blue balloon at index 2. This takes 3 seconds.
+// There are no longer two consecutive balloons of the same color. Total time = 3.
+// Example 2:
+
+
+// Input: colors = "abc", neededTime = [1,2,3]
+// Output: 0
+// Explanation: The rope is already colorful. Bob does not need to remove any balloons from the rope.
+// Example 3:
+
+
+// Input: colors = "aabaa", neededTime = [1,2,3,4,1]
+// Output: 2
+// Explanation: Bob will remove the balloons at indices 0 and 4. Each balloons takes 1 second to remove.
+// There are no longer two consecutive balloons of the same color. Total time = 1 + 1 = 2.
+ 
+
+// Constraints:
+
+// n == colors.length == neededTime.length
+// 1 <= n <= 10^5
+// 1 <= neededTime[i] <= 10^4
+// colors contains only lowercase English letters.
+
+
+// Solution: 
+
+
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int l=0, r;
+        final int n=colors.length();
+        int removes=0, locMax=0;
+        for (r=0; r<n; ){
+            while (r<n && colors.charAt(r)==colors.charAt(l)) {
+                removes+=neededTime[r];
+                locMax=Math.max(locMax, neededTime[r]);
+                r++;
+            }
+            removes-= locMax;
+            locMax=0;
+            l=r; 
+        }
+        return  removes;
+    }
+}
