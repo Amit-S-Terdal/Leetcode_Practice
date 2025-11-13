f1289b7eaaf9d011ac076d0227b6d0c785dc9c11
Amit Terdal <amitterdal2906@gmail.com>
2025-11-13 15:17:41 +0000

    Pushed through bash

---
+++ day_287.py
@@ -0,0 +1,58 @@
+# 3228. Maximum Number of Operations to Move Ones to the End
+
+# You are given a binary string s.
+
+# You can perform the following operation on the string any number of times:
+
+# Choose any index i from the string where i + 1 < s.length such that s[i] == '1' and s[i + 1] == '0'.
+# Move the character s[i] to the right until it reaches the end of the string or another '1'. For example, for s = "010010", if we choose i = 1, the resulting string will be s = "000110".
+# Return the maximum number of operations that you can perform.
+
+ 
+
+# Example 1:
+
+# Input: s = "1001101"
+
+# Output: 4
+
+# Explanation:
+
+# We can perform the following operations:
+
+# Choose index i = 0. The resulting string is s = "0011101".
+# Choose index i = 4. The resulting string is s = "0011011".
+# Choose index i = 3. The resulting string is s = "0010111".
+# Choose index i = 2. The resulting string is s = "0001111".
+# Example 2:
+
+# Input: s = "00111"
+
+# Output: 0
+
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 10^5
+# s[i] is either '0' or '1'.
+
+
+# Solution:
+
+class Solution(object):
+    def maxOperations(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        cnt = 0
+        n = len(s)
+        cnt1 = 1 if s[0] == '1' else 0
+        for i in range(1, n):
+            if s[i] == '1':
+                cnt1 += 1
+            elif s[i - 1] == '1':
+                cnt += cnt1
+        return cnt
+
