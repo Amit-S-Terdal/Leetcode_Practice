e064d0a368fdc57fae8a297ddc78fcc8dea2b435
Amit Terdal <amitterdal2906@gmail.com>
2025-11-15 12:10:36 +0000

    Pushed through bash

--- day_289.py
+++ day_289.py
@@ -53,44 +53,59 @@
 # Solution: 
 
 
+import bisect
+
 class Solution(object):
     def numberOfSubstrings(self, s):
         """
         :type s: str
         :rtype: int
         """
-        n = len(s)
-        Zmax = int(( (1 + 4*n)**0.5 - 1 ) // 2)
+        
+        n=len(s)
 
-        # p0 will store all positions of '0', plus a sentinel n
-        p0 = []
-        for i, ch in enumerate(s):
-            if ch == '0':
-                p0.append(i)
-        p0.append(n)
+        c1=[0]*(n+1)
 
-        ans = 0
-        front = 0
-        back = len(p0)
+        for i in range(1,n+1):
+            c1[i]=c1[i-1] + (1 if s[i-1]=='1' else 0)
 
-        for l in range(n):
-            # advance front past left boundary
-            while front < back and p0[front] < l:
-                front += 1
+        total_ones=c1[-1]
 
-            prev = l
+        next_zero=[0]*n
 
-            # iterate all possible zero-count levels
-            for p in range(front, back):
-                cnt0 = p - front
-                if cnt0 > Zmax:
+        curr_zero_pos=n
+
+        for i in range(n-1,-1,-1):
+            next_zero[i]=curr_zero_pos
+            if s[i]=='0':
+                curr_zero_pos=i
+
+        total=0
+
+        for i in range(n):
+            curr_zero_count= (1 if s[i]=='0' else 0)
+            curr_zero_pos=i
+            next_zero_pos=next_zero[i]
+
+            for cz in range(201):
+
+                if (total_ones-c1[i])<curr_zero_count**2:
                     break
 
-                r = p0[p]
-                minLen = max(prev - l + 1, cnt0 * (cnt0 + 1))
+                curr_valid_one_pos=bisect.bisect_left(
+                    c1,
+                    c1[i]+curr_zero_count**2,
+                    curr_zero_pos+1,
+                    next_zero_pos+1
+                )
 
-                ans += max(0, (r - l) - minLen + 1)
+                total+=next_zero_pos+1-curr_valid_one_pos
 
-                prev = r
+                if next_zero_pos==n:
+                    break
+                curr_zero_pos=next_zero_pos
+                next_zero_pos=next_zero[curr_zero_pos]
+                curr_zero_count+=1
 
-        return ans
+
+        return total
