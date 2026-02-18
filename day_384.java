6711a2b626a0668a63970ed220dddd33cb076f87
Amit S Terdal <amitterdal2906@gmail.com>
2026-02-18 07:44:48 +0000

    Pushed through bash

---
+++ day_384.java
@@ -0,0 +1,46 @@
+// 693. Binary Number with Alternating Bits
+
+// Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+
+ 
+
+// Example 1:
+
+// Input: n = 5
+// Output: true
+// Explanation: The binary representation of 5 is: 101
+// Example 2:
+
+// Input: n = 7
+// Output: false
+// Explanation: The binary representation of 7 is: 111.
+// Example 3:
+
+// Input: n = 11
+// Output: false
+// Explanation: The binary representation of 11 is: 1011.
+ 
+
+// Constraints:
+
+// 1 <= n <= 2^31 - 1
+
+
+
+
+// Solution: 
+
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int mB = 31 - Integer.numberOfLeadingZeros(n);
+        boolean prevB = true;
+
+        for (int i = mB - 1; i >= 0; i--, prevB = !prevB) {
+            if (((n >> i) & 1) == (prevB ? 1 : 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
