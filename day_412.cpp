2503c902ecbcfca36648f651cf06a434710deae5
Amit S Terdal <amitterdal2906@gmail.com>
2026-03-18 05:41:31 +0000

    Pushed through bash

---
+++ day_412.cpp
@@ -0,0 +1,67 @@
+// 3070. Count Submatrices with Top-Left Element and Sum Less Than k
+
+// You are given a 0-indexed integer matrix grid and an integer k.
+
+// Return the number of submatrices that contain the top-left element of the grid, and have a sum less than or equal to k.
+
+ 
+
+// Example 1:
+
+
+// Input: grid = [[7,6,3],[6,6,1]], k = 18
+// Output: 4
+// Explanation: There are only 4 submatrices, shown in the image above, that contain the top-left element of grid, and have a sum less than or equal to 18.
+// Example 2:
+
+
+// Input: grid = [[7,2,9],[1,5,0],[2,6,6]], k = 20
+// Output: 6
+// Explanation: There are only 6 submatrices, shown in the image above, that contain the top-left element of grid, and have a sum less than or equal to 20.
+ 
+
+// Constraints:
+
+// m == grid.length 
+// n == grid[i].length
+// 1 <= n, m <= 1000 
+// 0 <= grid[i][j] <= 1000
+// 1 <= k <= 10^9
+
+
+
+
+// Solution:
+
+
+
+class Solution {
+public:
+    static int countSubmatrices(vector<vector<int>>& grid, int k) {
+        const int r=grid.size(), c=grid[0].size();
+        int cnt=0, brCol=c;
+        if (grid[0][0]>k) return 0;// early stop
+        cnt++;
+        for(int j=1; j<c; j++){
+            int& x=grid[0][j];
+            x+=grid[0][j-1];
+            if(x>k)// no need for computing for the rest cols
+                { brCol=j; break;}
+            cnt++;
+        }
+        for(int i=1; i<r; i++){
+            grid[i][0]+=grid[i-1][0];
+            if (grid[i][0]>k) break;
+            cnt++;
+            for(int j=1; j<brCol; j++){
+                int& x=grid[i][j];
+                x+=grid[i-1][j]+grid[i][j-1]-grid[i-1][j-1];
+                if (x>k){
+                    brCol=j; break;
+                }
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
