0694c4a7b154898b0fece47f19df3f23bf0a178e
Amit S Terdal <amitterdal2906@gmail.com>
2026-03-30 01:33:17 +0000

    Pushed through bash

---
+++ day_424.cpp
@@ -0,0 +1,60 @@
+// 2840. Check if Strings Can be Made Equal With Operations II
+
+// You are given two strings s1 and s2, both of length n, consisting of lowercase English letters.
+
+// You can apply the following operation on any of the two strings any number of times:
+
+// Choose any two indices i and j such that i < j and the difference j - i is even, then swap the two characters at those indices in the string.
+// Return true if you can make the strings s1 and s2 equal, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s1 = "abcdba", s2 = "cabdab"
+// Output: true
+// Explanation: We can apply the following operations on s1:
+// - Choose the indices i = 0, j = 2. The resulting string is s1 = "cbadba".
+// - Choose the indices i = 2, j = 4. The resulting string is s1 = "cbbdaa".
+// - Choose the indices i = 1, j = 5. The resulting string is s1 = "cabdab" = s2.
+// Example 2:
+
+// Input: s1 = "abe", s2 = "bea"
+// Output: false
+// Explanation: It is not possible to make the two strings equal.
+ 
+
+// Constraints:
+
+// n == s1.length == s2.length
+// 1 <= n <= 10^5
+// s1 and s2 consist only of lowercase English letters.
+
+
+
+// Solution:
+
+
+
+
+class Solution {
+public:
+    static bool checkStrings(string& s1, string& s2) {
+        array<array<int, 26>, 2> freq{};
+        constexpr array<array<int, 26>, 2> zero{};
+        const int n=s1.size();
+        for(int i=0; i<n; i++){
+            bool iOdd=i&1;
+            freq[iOdd][s1[i]-'a']++;
+            freq[iOdd][s2[i]-'a']--;
+        }
+        return freq==zero;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
