7e8d74b24b33c442f5208df3086c0f67723683a5
Amit S Terdal <amitterdal2906@gmail.com>
2026-04-02 05:51:40 +0000

    Pushed through bash

---
+++ day_427.cpp
@@ -0,0 +1,106 @@
+// 3418. Maximum Amount of Money Robot Can Earn
+
+// You are given an m x n grid. A robot starts at the top-left corner of the grid (0, 0) and wants to reach the bottom-right corner (m - 1, n - 1). The robot can move either right or down at any point in time.
+
+// The grid contains a value coins[i][j] in each cell:
+
+// If coins[i][j] >= 0, the robot gains that many coins.
+// If coins[i][j] < 0, the robot encounters a robber, and the robber steals the absolute value of coins[i][j] coins.
+// The robot has a special ability to neutralize robbers in at most 2 cells on its path, preventing them from stealing coins in those cells.
+
+// Note: The robot's total coins can be negative.
+
+// Return the maximum profit the robot can gain on the route.
+
+ 
+
+// Example 1:
+
+// Input: coins = [[0,1,-1],[1,-2,3],[2,-3,4]]
+
+// Output: 8
+
+// Explanation:
+
+// An optimal path for maximum coins is:
+
+// Start at (0, 0) with 0 coins (total coins = 0).
+// Move to (0, 1), gaining 1 coin (total coins = 0 + 1 = 1).
+// Move to (1, 1), where there's a robber stealing 2 coins. The robot uses one neutralization here, avoiding the robbery (total coins = 1).
+// Move to (1, 2), gaining 3 coins (total coins = 1 + 3 = 4).
+// Move to (2, 2), gaining 4 coins (total coins = 4 + 4 = 8).
+// Example 2:
+
+// Input: coins = [[10,10,10],[10,10,10]]
+
+// Output: 40
+
+// Explanation:
+
+// An optimal path for maximum coins is:
+
+// Start at (0, 0) with 10 coins (total coins = 10).
+// Move to (0, 1), gaining 10 coins (total coins = 10 + 10 = 20).
+// Move to (0, 2), gaining another 10 coins (total coins = 20 + 10 = 30).
+// Move to (1, 2), gaining the final 10 coins (total coins = 30 + 10 = 40).
+ 
+
+// Constraints:
+
+// m == coins.length
+// n == coins[i].length
+// 1 <= m, n <= 500
+// -1000 <= coins[i][j] <= 1000
+
+
+
+// Solution:
+
+
+
+
+static int dp[500][3];
+
+class Solution {
+public:
+    static int maximumAmount(vector<vector<int>>& coins) {
+        const int r=coins.size(), c=coins[0].size();
+        // Base case
+        int x=coins[0][0];
+        dp[0][0]=x;          
+        dp[0][1]=dp[0][2]=max(0, x);  
+
+        for(int j=1; j<c; j++){// 0th row
+            x=coins[0][j];
+            for (int k=2; k>0; k--){
+                dp[j][k]=max(x+dp[j-1][k], dp[j-1][k-1]);
+            }
+            dp[j][0]=x+dp[j-1][0];// k=0
+        }
+
+        for (int i=1; i<r; i++) {
+            // 0th col
+            x=coins[i][0];
+            for (int k=2; k>0; k--){
+                dp[0][k]=max(x+dp[0][k], dp[0][k-1]);
+            }
+            dp[0][0]=x+dp[0][0];// k=0
+            
+            for (int j=1; j<c; j++) {
+                x=coins[i][j];
+                for (int k=2; k>0; k--) {
+                    dp[j][k]=max(max(x+dp[j-1][k], dp[j-1][k-1]), max(x+dp[j][k], dp[j][k-1]));
+                }
+                dp[j][0]=x+max(dp[j][0], dp[j-1][0]);// k=0
+
+            }
+        }
+        return dp[c-1][2];
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
