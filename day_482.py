3050d80efa896661841d7d6eca96a4a7bcbc0327
Amit S Terdal <amitterdal2906@gmail.com>
2026-05-27 02:34:49 +0000

    Pushed through bash

--- day_482.py
+++ day_482.py
@@ -1,42 +1,48 @@
-# 1340. Jump Game V
+# 3121. Count the Number of Special Characters II
 
-# Given an array of integers arr and an integer d. In one step you can jump from index i to index:
+# You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word, and every lowercase occurrence of c appears before the first uppercase occurrence of c.
 
-# i + x where: i + x < arr.length and  0 < x <= d.
-# i - x where: i - x >= 0 and  0 < x <= d.
-# In addition, you can only jump from index i to index j if arr[i] > arr[j] and arr[i] > arr[k] for all indices k between i and j (More formally min(i, j) < k < max(i, j)).
-
-# You can choose any index of the array and start jumping. Return the maximum number of indices you can visit.
-
-# Notice that you can not jump outside of the array at any time.
+# Return the number of special letters in word.
 
  
 
 # Example 1:
 
+# Input: word = "aaAbcBC"
 
-# Input: arr = [6,4,14,6,8,13,9,7,10,6,12], d = 2
-# Output: 4
-# Explanation: You can start at index 10. You can jump 10 --> 8 --> 6 --> 7 as shown.
-# Note that if you start at index 6 you can only jump to index 7. You cannot jump to index 5 because 13 > 9. You cannot jump to index 4 because index 5 is between index 4 and 6 and 13 > 9.
-# Similarly You cannot jump from index 3 to index 2 or index 1.
+# Output: 3
+
+# Explanation:
+
+# The special characters are 'a', 'b', and 'c'.
+
 # Example 2:
 
-# Input: arr = [3,3,3,3,3], d = 3
-# Output: 1
-# Explanation: You can start at any index. You always cannot jump to any index.
+# Input: word = "abc"
+
+# Output: 0
+
+# Explanation:
+
+# There are no special characters in word.
+
 # Example 3:
 
-# Input: arr = [7,6,5,4,3,2,1], d = 1
-# Output: 7
-# Explanation: Start at index 0. You can visit all the indicies. 
+# Input: word = "AbBCab"
+
+# Output: 0
+
+# Explanation:
+
+# There are no special characters in word.
+
  
 
 # Constraints:
 
-# 1 <= arr.length <= 1000
-# 1 <= arr[i] <= 10^5
-# 1 <= d <= arr.length
+# 1 <= word.length <= 2 * 10^5
+# word consists of only lowercase and uppercase English letters.
+
 
 
 
@@ -46,65 +52,16 @@
 
 
 class Solution(object):
-    def maxJumps(self, arr, d):
+    def numberOfSpecialChars(self, word):
         """
-        :type arr: List[int]
-        :type d: int
+        :type word: str
         :rtype: int
         """
+        A = [[False, False] for _ in range(27)]
 
-        n = len(arr)
+        for ch in word:
+            i = ord(ch) & 31
+            c = (ord(ch) >> 5) & 1
+            A[i][c] = not (c and A[i][0])
 
-        adj = [[] for _ in range(n)]
-        deg = [0] * n
-        dp = [1] * n
-
-        def add_edge(u, v):
-            adj[u].append(v)
-            deg[v] += 1
-
-        stack = []
-
-        # left -> right monotonic stack
-        for i in range(n):
-            while stack and arr[stack[-1]] < arr[i]:
-                j = stack.pop()
-
-                if i - j <= d:
-                    add_edge(j, i)
-
-            stack.append(i)
-
-        # right -> left monotonic stack
-        stack = []
-
-        for i in range(n - 1, -1, -1):
-            while stack and arr[stack[-1]] < arr[i]:
-                j = stack.pop()
-
-                if j - i <= d:
-                    add_edge(j, i)
-
-            stack.append(i)
-
-        # topological BFS
-        from collections import deque
-
-        q = deque()
-
-        for i in range(n):
-            if deg[i] == 0:
-                q.append(i)
-
-        while q:
-            u = q.popleft()
-
-            for v in adj[u]:
-                dp[v] = max(dp[v], dp[u] + 1)
-
-                deg[v] -= 1
-
-                if deg[v] == 0:
-                    q.append(v)
-
-        return max(dp)
+        return sum(u and v for u, v in A)
