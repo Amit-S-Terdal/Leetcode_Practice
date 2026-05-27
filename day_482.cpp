528ee26d1b32e438069cc4a2a152d0f94a8a7992
Amit S Terdal <amitterdal2906@gmail.com>
2026-05-27 02:34:28 +0000

    Pushed through bash

--- day_482.cpp
+++ day_482.cpp
@@ -1,42 +1,48 @@
-// 1340. Jump Game V
+// 3121. Count the Number of Special Characters II
 
-// Given an array of integers arr and an integer d. In one step you can jump from index i to index:
+// You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word, and every lowercase occurrence of c appears before the first uppercase occurrence of c.
 
-// i + x where: i + x < arr.length and  0 < x <= d.
-// i - x where: i - x >= 0 and  0 < x <= d.
-// In addition, you can only jump from index i to index j if arr[i] > arr[j] and arr[i] > arr[k] for all indices k between i and j (More formally min(i, j) < k < max(i, j)).
-
-// You can choose any index of the array and start jumping. Return the maximum number of indices you can visit.
-
-// Notice that you can not jump outside of the array at any time.
+// Return the number of special letters in word.
 
  
 
 // Example 1:
 
+// Input: word = "aaAbcBC"
 
-// Input: arr = [6,4,14,6,8,13,9,7,10,6,12], d = 2
-// Output: 4
-// Explanation: You can start at index 10. You can jump 10 --> 8 --> 6 --> 7 as shown.
-// Note that if you start at index 6 you can only jump to index 7. You cannot jump to index 5 because 13 > 9. You cannot jump to index 4 because index 5 is between index 4 and 6 and 13 > 9.
-// Similarly You cannot jump from index 3 to index 2 or index 1.
+// Output: 3
+
+// Explanation:
+
+// The special characters are 'a', 'b', and 'c'.
+
 // Example 2:
 
-// Input: arr = [3,3,3,3,3], d = 3
-// Output: 1
-// Explanation: You can start at any index. You always cannot jump to any index.
+// Input: word = "abc"
+
+// Output: 0
+
+// Explanation:
+
+// There are no special characters in word.
+
 // Example 3:
 
-// Input: arr = [7,6,5,4,3,2,1], d = 1
-// Output: 7
-// Explanation: Start at index 0. You can visit all the indicies. 
+// Input: word = "AbBCab"
+
+// Output: 0
+
+// Explanation:
+
+// There are no special characters in word.
+
  
 
 // Constraints:
 
-// 1 <= arr.length <= 1000
-// 1 <= arr[i] <= 10^5
-// 1 <= d <= arr.length
+// 1 <= word.length <= 2 * 10^5
+// word consists of only lowercase and uppercase English letters.
+
 
 
 
@@ -45,73 +51,18 @@
 
 
 
-struct Edge{
-    int to, nxt=-1;
-};
-
-constexpr int E=1000*999/2;// at most C(1000, 2) edges
-constexpr int V=1000;
-
-Edge POOL[E];
-int idx=0;
-
-int adj[V], deg[V];
-int dp[V];
-
-int q[V];// for queue holding idx
-int front, back;
-
-inline void addEdge(int u, int v){
-    POOL[idx]={v, adj[u]};
-    adj[u]=idx++;
-    deg[v]++;
-}
-int Stack[V], top=-1;
-
 class Solution {
 public:
-    static int maxJumps(vector<int>& arr, int d) {
-        const int n=arr.size();
-        // reset
-        idx=0;
-        memset(adj, -1, n*sizeof(int));
-        memset(deg, 0, n*sizeof(int));
-        fill(dp, dp+n, 1);
-        // montonone stack
-        top=-1;// clear stack
-        for(int i=0; i<n; i++){
-            const int x=arr[i];
-            while(top>-1 && arr[Stack[top]]<x){
-                int j=Stack[top--];
-                if (i-j<=d) addEdge(j, i);
-            }
-            Stack[++top]=i;
-        }
-        top=-1;// clear Stack
-        for(int i=n-1; i>=0; i--){
-            const int x=arr[i];
-            while(top>-1 && arr[Stack[top]]<x){
-                int j=Stack[top--];
-                if (j-i<=d) addEdge(j, i);
-            }
-            Stack[++top]=i;
+    int numberOfSpecialChars(string word) {
+        bitset<27> A[2];
+
+        for (auto& ch : word) {
+            int i = ch & 31;
+            int Case = (ch >> 5) & 1;
+
+            A[Case].set(i, !(Case & A[0][i]));
         }
 
-        front=back= 0;// reset for q
-        for(int i=0; i<n; i++)// Push i to q if deg[i]=0
-            if(deg[i]==0)
-                q[back++]=i;
-
-        while(front<back){
-            int u=q[front++];
-            for(int e=adj[u]; e!=-1; e=POOL[e].nxt){
-                int v=POOL[e].to;
-                dp[v]=max(dp[v], dp[u]+1);
-                if(--deg[v]==0)
-                    q[back++]=v;
-            }
-        }
-
-        return *max_element(dp, dp+n);
+        return (A[0] & A[1]).count();
     }
 };
