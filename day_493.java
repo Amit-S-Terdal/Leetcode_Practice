110753e4865180b180331d7a8ca1eed1a520aa73
Amit S Terdal <amitterdal2906@gmail.com>
2026-06-07 04:05:39 +0000

    Pushed through bash

---
+++ day_493.java
@@ -0,0 +1,105 @@
+// 2196. Create Binary Tree From Descriptions
+
+// You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+// If isLefti == 1, then childi is the left child of parenti.
+// If isLefti == 0, then childi is the right child of parenti.
+// Construct the binary tree described by descriptions and return its root.
+
+// The test cases will be generated such that the binary tree is valid.
+
+ 
+
+// Example 1:
+
+
+// Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+// Output: [50,20,80,15,17,19]
+// Explanation: The root node is the node with value 50 since it has no parent.
+// The resulting binary tree is shown in the diagram.
+// Example 2:
+
+
+// Input: descriptions = [[1,2,1],[2,3,0],[3,4,1]]
+// Output: [1,2,null,null,3,4]
+// Explanation: The root node is the node with value 1 since it has no parent.
+// The resulting binary tree is shown in the diagram.
+ 
+
+// Constraints:
+
+// 1 <= descriptions.length <= 10^4
+// descriptions[i].length == 3
+// 1 <= parenti, childi <= 10^5
+// 0 <= isLefti <= 1
+// The binary tree described by descriptions is valid.
+
+
+
+
+// Solution:
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        final int N = 100001;
+
+        boolean[] seen = new boolean[N];
+        int[] parent = new int[N];
+        TreeNode[] node = new TreeNode[N];
+
+        int root = -1;
+
+        for (int[] d : descriptions) {
+            int x = d[0];
+            int y = d[1];
+            int l = d[2];
+
+            if (!seen[x]) {
+                node[x] = new TreeNode(x);
+                seen[x] = true;
+
+                if (parent[x] == 0) {
+                    root = x;
+                }
+            }
+
+            if (!seen[y]) {
+                node[y] = new TreeNode(y);
+                seen[y] = true;
+            }
+
+            parent[y] = x;
+
+            if (l == 1) {
+                node[x].left = node[y];
+            } else {
+                node[x].right = node[y];
+            }
+        }
+
+        while (parent[root] != 0) {
+            root = parent[root];
+        }
+
+        return node[root];
+    }
+}
